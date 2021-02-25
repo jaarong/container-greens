940661afee10eb134e4a3dd34bb763213c14e83d
jaarong <jaarongreen@gmail.com>
2021-02-25 16:38:40 -0600

    Update meta.js

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,8 +1,8 @@
 module.exports = {
-	siteURL: process.env.URL || 'http://localhost:8080',
+	siteURL: process.env.URL || 'https://container-garden.netlify.com',
 	siteName: "Container.Garden",
 	siteDescription:
-		'A highly configurable 11ty template to quickly spin up a fully functional website with TailwindCSS and Alpine.js baked in.',
+		'My adventures in container gardening',
 	siteImage: '/assets/images/site/leaf_logo.jpg',
 	lang: 'en',
 	locale: 'en_us',
